6f7d93ab431b7deaf38df10a58dbcdadf1347ea4
Muhamad Zidan <aldy10ball@gmail.com>
2025-09-12 14:43:01 +0700

    Time: 4 ms (84.46%), Space: 45 MB (67.68%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,24 +1,28 @@
 class Solution {
     public String convert(String s, int numRows) {
-        String result = "";
-        String rowString[] = new String[numRows];
+        
         int idx = 0;
         int dir = 1; 
-
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
         if (numRows == 1){
             return s;
         }
-        for (int i = 0; i < numRows; i++) {
-                rowString[i]  = ""; // initialize with empty string
-        }
          for (int i = 0; i < s.length(); i++){
-            rowString[idx] += (s.charAt(i));
+             rows[idx].append(s.charAt(i));
             idx += dir;
             if (idx == numRows-1 || idx == 0) {
                 dir *= -1; // reverse direction
             }
-        }   
+        }  
 
-        return String.join("", rowString);
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
     }
 }
