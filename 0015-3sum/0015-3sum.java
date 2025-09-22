51888c4f9b5dc5b58257a5659bf2214d9f70fa68
Muhamad Zidan <aldy10ball@gmail.com>
2025-09-22 14:17:40 +0700

    Time: 0 ms (100%), Space: 53 MB (11.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,83 +1,44 @@
+import java.util.AbstractList;
 class Solution {
+    private List<List<Integer>> res;
     public List<List<Integer>> threeSum(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
-        List<List<Integer>> result = new ArrayList<>();
-
-        for (int i = 0; i < nums.length; i++){
-            if (i > 0 && nums[i] == nums[i-1]){
-                continue;
+        int target = 0;
+        return new AbstractList<List<Integer>>() {
+            public List<Integer> get(int index) {
+                init();
+                return res.get(index);
             }
-            int k = nums.length - 1;
-            int j = i + 1 ;
-
-
-            while(j<k){
-                int total = nums[i] + nums[j] + nums[k];
-                if(nums[k] < Math.abs(nums[i]/2)){
-                    break;
-                }
-                if ( total> 0){
-                    k--;
-                } else if ( total < 0){
-                    j++;
-                } else {
-                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    while(nums[j] == nums[j-1] && j<k){
-                        j++;
+            public int size() {
+                init();
+                return res.size();
+            }
+            private void init() {
+                if (res != null) return;
+                Arrays.sort(nums);
+                int l, r;
+                int sum;
+                Set<List<Integer>> tempRes = new HashSet<>();
+                for (int i = 0; i < nums.length - 2; ++i) {
+                    l = i + 1;
+                    r = nums.length - 1;
+                    while (l < r) {
+                        sum = nums[i] + nums[l] + nums[r];
+                        if(nums[r] < Math.abs(nums[i]/2)){
+                            break;
+                        }
+                        if (sum == target) {
+                            List<Integer> t = new ArrayList<>();
+                            t.add(nums[i]);
+                            t.add(nums[l]);
+                            t.add(nums[r]);
+                            tempRes.add(t);
+                        }
+                        if (sum < target) ++l;
+                        else --r;
                     }
                 }
+                res = new ArrayList<List<Integer>>(tempRes);
             }
-
-            
-        }
-        return result;
-    }
-
-     // partition function
-    static int partition(int[] arr, int low, int high) {
-        
-        // choose the pivot
-        int pivot = arr[high];
-        
-        // index of smaller element and indicates 
-        // the right position of pivot found so far
-        int i = low - 1;
-
-        // traverse arr[low..high] and move all smaller
-        // elements to the left side. Elements from low to 
-        // i are smaller after every iteration
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        
-        // Move pivot after smaller elements and
-        // return its position
-        swap(arr, i + 1, high);  
-        return i + 1;
-    }
-
-    // swap function
-    static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    // the QuickSort function implementation
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            
-            // pi is the partition return index of pivot
-            int pi = partition(arr, low, high);
-
-            // recursion calls for smaller elements
-            // and greater or equals elements
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
+        };
     }
 }
