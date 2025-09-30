f9132963929d9357675a24f60bfb160b3d64968f
Muhamad Zidan <aldy10ball@gmail.com>
2025-09-30 16:46:11 +0700

    Time: 1 ms (58.5%), Space: 45 MB (41.98%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,8 +3,7 @@ class Solution {
         int k = 1;
 
         for(int i = 1; i < nums.length; i++){
-            if (nums[i-1] == nums[i]){
-            } else {
+            if (nums[i-1] != nums[i]){
                 nums[k] = nums[i];
                 k++;
             }
