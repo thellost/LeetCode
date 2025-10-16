b8a03fc816a029c54dc0cc288fa56d134ea5e9aa
Muhamad Zidan <aldy10ball@gmail.com>
2025-10-16 13:16:03 +0700

    Time: 0 ms (100%), Space: 43.3 MB (19.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -6,25 +6,18 @@ class Solution {
         while (l <= r) {
             int m = (l + r) / 2;
 
-            // Index of Element Returned
             if (nums[m] == target) {
                 return m;
 
-            // If element is smaller than mid, then
-            // it can only be present in left subarray
-            // so we decrease our r pointer to mid - 1 
             } else if (nums[m] > target) {
                 r = m - 1;
 
-            // Else the element can only be present
-            // in right subarray
-            // so we increase our l pointer to mid + 1
             } else {
               l = m + 1;
             }  
         }
 
-        // No Element Found
+        // No Element Found, Return left pointer;
         return l;
     }
 }
